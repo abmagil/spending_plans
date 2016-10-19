b1e7fb6ee564aecbc1e297553d233824bbbf4587
Aaron Magil <aaron@societyofgrownups.com>
2016-10-19 17:14:30 -0400

    Add MonthlyAvailableCash
    The table must know how much cash is available monthly in order to calc
    when a user has overspent.  This number is given by the user as a basic
    input
    
    This introduces state at the app-level, so App.js is converted to class

--- src/App.js
+++ src/App.js
@@ -3,13 +3,42 @@ import FrpTable from './components/FrpTable'
 
 import React from 'react'
 
-let App = React.createClass({
+const {
+  Component
+} = React;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monthlyAvailableCash: 400
+    };
+  }
+
+  updateMonthlyAvailable = () => {
+    let monthlyAvailableCash = this.monthlyAvailableInput.value;
+
+    this.setState({
+      ...this.state,
+      monthlyAvailableCash
+    })
+  }
+
   render() {
+    let { monthlyAvailableCash } = this.state;
+
     return <div>
       <h1>FRP Goals in React</h1>
-      <FrpTable />
+      <label>Monthly Available Cash
+        <input type="number"
+          ref={c => this.monthlyAvailableInput = c}
+          value={monthlyAvailableCash}
+          onChange={this.updateMonthlyAvailable}
+        />
+      </label>
+      <FrpTable available={monthlyAvailableCash}/>
     </div>
   }
-})
+}
 
 export default App
