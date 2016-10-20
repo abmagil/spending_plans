b33e308bcc86f890cfac976f5fd4e69deed32d9b
Aaron Magil <aaron@societyofgrownups.com>
2016-10-20 17:12:52 -0400

    Error handle in cdf.  Test cdf

---
+++ tests/cdf-test.js
@@ -0,0 +1,26 @@
+import cdf from '../cdf';
+import expect from 'expect';
+
+describe('CDF function', () => {
+  it('accumulates values from an array into a new array of the same length', () => {
+    expect(cdf([1,1,1,1])).toEqual([1, 2, 3, 4]);
+  });
+
+  it('coerces numeric array elements to numbers', () => {
+    expect(cdf(['1.5', 1, 1.5, [1]])).toEqual([1.5, 2.5, 4, 5]);
+  });
+
+  it('errors on non-numeric array elements', () => {
+    expect(() => cdf([{a: 1}])).toThrow(/cdf requires an array of numbers/, 'handles objects');
+    expect(() => cdf(["a"])).toThrow(/cdf requires an array of numbers/, 'handles non-numeric strings');
+    expect(() => cdf(["a1"])).toThrow(/cdf requires an array of numbers/, 'handles non-numeric strings');
+    expect(() => cdf(["1a"])).toThrow(/cdf requires an array of numbers/, 'handles non-numeric strings');
+    expect(() => cdf([true])).toThrow(/cdf requires an array of numbers/, 'handles true');
+    expect(() => cdf([false])).toThrow(/cdf requires an array of numbers/, 'handles false');
+    expect(() => cdf([["a"]])).toThrow(/cdf requires an array of numbers/, 'handles arrays of arrays');
+  });
+
+  it('errors when passed a non-array argument', () => {
+    expect(() => cdf(true)).toThrow(/cdf requires an array of number/)
+  })
+});
